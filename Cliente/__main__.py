36db02b207b58c7c59da9c8e5005a9890fbedd49
ababa <2024111736@uac.pt>
2026-05-17 17:45:05 +0000

    Add files via upload

---
+++ Cliente/__main__.py
@@ -0,0 +1,11 @@
+
+from interface.interface import Interface
+def main():
+    print("Executing Main in cliente")
+    #maq = Maquina()
+    #int = Interface(maq)
+    int = Interface()
+    int.execute()
+
+if __name__ == '__main__':
+    main()
